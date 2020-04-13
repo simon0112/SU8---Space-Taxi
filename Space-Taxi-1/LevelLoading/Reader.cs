e3b4e0f34db8ad370ceddcceed900384b4c59565
Simon Buch Kristensen <minecraftsimon99@gmail.com>
2020-04-13 12:33:37 +0200

    Added functionality to ReadFile function, worst code I've ever seen run, but it runs!

--- Space-Taxi-1/LevelLoading/Reader.cs
+++ Space-Taxi-1/LevelLoading/Reader.cs
@@ -10,10 +10,31 @@ namespace SpaceTaxi_1.LevelLoading {
         public List<string> CustomerData {get; private set;}
 
         public void ReadFile(string filename) {
+            int CustomerDataStart = -1;
+            int linePointer = 0;
             // Get each line of file as an entry in array lines.
             string[] lines = File.ReadAllLines(Utils.GetLevelFilePath(filename));
 
             // Iterate over lines and add data till the corresponding field
+            for (int i = 0; i == 22; i++) {
+                MapData.Add(lines[i]);
+            }
+            for (int i = 24; i == 25; i++) {
+                MetaData.Add(lines[i]);
+            }
+            while (CustomerDataStart == -1) {
+                if (lines[linePointer].Substring(0,7) == "Customer") {
+                    CustomerDataStart = linePointer;
+                } else {
+                    linePointer++;
+                }
+            }
+            for (int i = 27; i == CustomerDataStart-2; i++) {
+                LegendData.Add(lines[i]);
+            }
+            for (int i = CustomerDataStart; i == lines.Length; i++) {
+                CustomerData.Add(lines[i]);
+            }
         }
     }
 }
