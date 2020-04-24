871ee04bdb97482d66e8c48f7fbad878a1425cf8
Simon Buch Kristensen <minecraftsimon99@gmail.com>
2020-04-24 13:54:34 +0200

    rewrote som XML documentation

--- Space-Taxi-1/Entities/Player.cs
+++ Space-Taxi-1/Entities/Player.cs
@@ -11,19 +11,19 @@ namespace SpaceTaxi_1.Entities {
         public Orientation orientation {get; private set;}
         public Entity Entity {get; private set;}
 
-        //<summary> player constructor <summary/>
-        //<input> type = DynamicShape, name = shape </insput>
-        //<return>  </insput> 
+        ///<summary> player constructor <summary/>
+        ///<variable name="shape"> The Dynamic shape that the player is, the players placement and bounds</variable>
+        ///<variable name="Entity"> The entity that the player is, makes use of the shape, and an image path to create a visible player </variable>
+        ///<variable name="Orientation"> The orientation of the player, can either be left or rigtht</variable>
+        ///<returns> void, but instantiates the player object </returns> 
         public Player(DynamicShape shape) {
 
             Entity = new Entity(shape, new DIKUArcade.Graphics.Image(Path.Combine("Assets","Images","Taxi_Thrust_None_Right.png")));
             orientation = new Orientation();
-            // Entity.Shape.AsDynamicShape().Direction = (new Vec2F(0,(float) 0.0011));
         }
-        //<summary> sets the Orientation value to either left or right,
-        // depending on what image we want to be displayed. <summary/>
-        //<input> type = Orientation, name = value </insput>
-        //<return> void </insput> 
+        ///<summary> sets the Orientation value to either left or right, depending on what image we want to be displayed. <summary/>
+        ///<variable name="value"> A value using the Orientation enumeration.</variable>
+        ///<return> void, but changes values of the player object </returns> 
         public void playerIsLeftOrRight(Orientation value)
         {
             if (value == Orientation.Left)
@@ -39,11 +39,10 @@ namespace SpaceTaxi_1.Entities {
             }
         }
         
-        //<summary> executeds the code that is deceiding if the left, right, down or up key is pressed.
-        // and then deceiding how long the vectors is moving our player, and sets the right image
-        // using the playerIsLeftOrRight() methode. <summary/>
-        //<input> type = gameEventType and GameEvent<object> , name = gameEvent and eventType </insput>
-        //<return> void </insput> 
+        ///<summary> The processEvent that is related to the player, it processes playerevents based on what the message of that event is in the eventBus <summary/>
+        ///<variable name="eventType"> The event type that is related to the specific game event in the eventBus</variable>
+        ///<variable name="gameEvent"> The game event itself </variable>
+        ///<return> void, but runs functions depending on what the event consists of </insput> 
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
             Orientation value = Orientation.Left;
             if (eventType == GameEventType.PlayerEvent) {
