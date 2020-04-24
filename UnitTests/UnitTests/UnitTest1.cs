1ac5a6cd2d82ddb5f381124da495f1bd7e744e98
Anton <anton@makki.dk>
2020-04-24 12:46:24 +0200

    fixed xml tags

--- UnitTests/UnitTests/UnitTest1.cs
+++ UnitTests/UnitTests/UnitTest1.cs
@@ -4,24 +4,37 @@ using DIKUArcade;
 using SpaceTaxi_1.Entities;
 using SpaceTaxi_1.Enums;
 using DIKUArcade.Math;
+using System.IO;
+using System.Reflection;
+using DIKUArcade.EventBus;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+
 
 namespace UnitTests
 {
     public class Tests
     {
-        Orientation hey;
-        Orientation orientation;
+        
         [SetUp]
         public void Setup()
         {
-            
+
         }
 
         [Test]
         public void TestOfPlayerIsLeftOrRight()
         {
-            var player = new Player(new DIKUArcade.Entities.DynamicShape(0f,0f,0f,0f));
-            player.playerIsLeftOrRight(hey);
+            //
+            GameEventType eventType = new GameEventType();
+            GameEvent<object> gameEvent = new GameEvent<object>();
+            gameEvent.Message = "BOOSTER_TO_LEFT";
+            var player = new Player(new DynamicShape(10.0f,10.0f,10.0f,10.0f));
+            player.ProcessEvent(eventType, gameEvent);
+
+            string path = Directory.GetCurrentDirectory();
+            System.Console.WriteLine(path);
+
             System.Console.WriteLine("hi");
             Assert.AreEqual(1,1);
         }
