39a6d749f5d0f282e7a82cb08d9c3de1d6160872
Anton <anton@makki.dk>
2020-05-06 21:45:44 +0200

    fixed animations

--- Space-Taxi-1/Entities/Player.cs
+++ Space-Taxi-1/Entities/Player.cs
@@ -10,19 +10,14 @@ using System.Collections.Generic;
 
 namespace SpaceTaxi_1.Entities {
     public class Player : IGameEventProcessor<object> {
-        public Orientation orientation {get; private set;}
-<<<<<<< HEAD
-        public Entity entity {get; private set;}
+        public Orientation orientation { get; private set; }
+        public Entity entity { get; private set; }
         private int UpdatesSinceLastMovement;
-        private Vec2F Gravity = new Vec2F(0f,-0.0000005f);
+        private Vec2F Gravity = new Vec2F(0f, -0.0000005f);
         private ImageStride imageStride;
         public List<Image> playerStrides;
-
-=======
         private MoveDir moveDir;
-        public Entity Entity {get; private set;}
-        private Vec2F Gravity = new Vec2F(0f,-0.000005f);
->>>>>>> ff80e1873717e6fd7e6e651c760e77bcb241ebbc
+        public Entity Entity { get; private set; }
 
         ///<summary> player constructor <summary/>
         ///<variable name="shape"> The Dynamic shape that the player is, the players placement and bounds</variable>
@@ -68,69 +63,65 @@ namespace SpaceTaxi_1.Entities {
             Direction(this.entity.Shape.AsDynamicShape().Direction + (value + Gravity) * UpdateAmt);
         }
 
-<<<<<<< HEAD
-        public void GravityEffect()
-        {
-            Direction(this.entity.Shape.AsDynamicShape().Direction + (Gravity) * 1);
-=======
+       
         private void PhysicsEffect() {
             switch (moveDir) {
                 case MoveDir.None:
-                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (Gravity)*1);
+                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (Gravity) * 1);
                     break;
                 case MoveDir.Left:
-                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(-0.000040f, 0.0000f)+Gravity)*1);
+                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(-0.000040f, 0.0000f) + Gravity) * 1);
                     break;
                 case MoveDir.Right:
-                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(0.000040f, 0.0000f)+Gravity)*1);
+                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(0.000040f, 0.0000f) + Gravity) * 1);
                     break;
                 case MoveDir.Up:
-                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(0.0000f, 0.00004f)+Gravity)*1);
+                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(0.0000f, 0.00004f) + Gravity) * 1);
                     break;
                 case MoveDir.LeftUp:
-                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(-0.00002828f, 0.00002828f)+Gravity)*1);
+                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(-0.00002828f, 0.00002828f) + Gravity) * 1);
                     break;
                 case MoveDir.RightUp:
-                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(0.00002828f, 0.00002828f)+Gravity)*1);
+                    Direction(this.Entity.Shape.AsDynamicShape().Direction + (new Vec2F(0.00002828f, 0.00002828f) + Gravity) * 1);
                     break;
-            }
-            Move();
->>>>>>> ff80e1873717e6fd7e6e651c760e77bcb241ebbc
-        }
-
-        //<summary> this methode helps us make sure that the player is only running inside
-        // the screen space. So the player stops moving if it reach one of the sides. <summary/>
-        //<input> none </insput>
-        //<return> void </insput> 
-        public void Move()
-        {
-            if (entity.Shape.Position.X > 0f && entity.Shape.Position.X < 0.97f)
-            {
-                entity.Shape.Move();
-            }
-            else if (entity.Shape.Position.X <= 0f && entity.Shape.AsDynamicShape().Direction.X > 0f)
-            {
-                entity.Shape.Move();
-            }
-            else if (entity.Shape.Position.X >= 0.9f && entity.Shape.AsDynamicShape().Direction.X < 0f)
-            {
-                entity.Shape.Move();
-            }
-            else
-            {
-                this.Direction(new Vec2F(0f, 0f));
+                }
+                Move();
             }
 
-            if (entity.Shape.Position.Y > -0.005f && entity.Shape.Position.Y < 0.955f)
+            //<summary> this methode helps us make sure that the player is only running inside
+            // the screen space. So the player stops moving if it reach one of the sides. <summary/>
+            //<input> none </insput>
+            //<return> void </insput> 
+            public void Move()
             {
-                entity.Shape.Move();
+                if (entity.Shape.Position.X > 0f && entity.Shape.Position.X < 0.97f)
+                {
+                    entity.Shape.Move();
+                }
+                else if (entity.Shape.Position.X <= 0f && entity.Shape.AsDynamicShape().Direction.X > 0f)
+                {
+                    entity.Shape.Move();
+                }
+                else if (entity.Shape.Position.X >= 0.9f && entity.Shape.AsDynamicShape().Direction.X < 0f)
+                {
+                    entity.Shape.Move();
+                }
+                else
+                {
+                    this.Direction(new Vec2F(0f, 0f));
+                }
 
+                if (entity.Shape.Position.Y > -0.005f && entity.Shape.Position.Y < 0.955f)
+                {
+                    entity.Shape.Move();
+
+                }
+                else
+                {
+                    this.Direction(new Vec2F(0f, 0f));
+                }
             }
-             else
-            {
-                this.Direction(new Vec2F(0f, 0f));
-            }
-        }
+
 
 
 
@@ -141,19 +132,12 @@ namespace SpaceTaxi_1.Entities {
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent)
         {
             Orientation value = Orientation.Left;
-<<<<<<< HEAD
             if (eventType == GameEventType.TimedEvent)
             {
                 switch (gameEvent.Message)
                 {
                     case "UPDATE_AMT_DELIVERY":
                         UpdatesSinceLastMovement = (int.Parse(gameEvent.Parameter1)) / 60;
-=======
-            if (eventType == GameEventType.TimedEvent) {
-                switch (gameEvent.Message) {
-                    case "UPDATE_PHYSICS":
-                        PhysicsEffect();
->>>>>>> ff80e1873717e6fd7e6e651c760e77bcb241ebbc
                         break;
                 }
             }
@@ -163,7 +147,6 @@ namespace SpaceTaxi_1.Entities {
                 {
                     case "BOOSTER_TO_LEFT":
                         value = Orientation.Left;
-<<<<<<< HEAD
                         playerIsLeftOrRight(value);
                         playerStrides = ImageStride.CreateStrides(2,
                         Path.Combine("Assets", "Images", "Taxi_Thrust_Back.png"));
@@ -185,19 +168,6 @@ namespace SpaceTaxi_1.Entities {
                         System.Console.WriteLine(value);
                         switch (value)
                         {
-=======
-                        this.Entity.Image = new DIKUArcade.Graphics.Image(Path.Combine("Assets", "Images", "Taxi_Thrust_Back.png"));
-                        moveDir = MoveDir.Left;
-                        break;
-                    case "BOOSTER_TO_RIGHT":
-                        value = Orientation.Right;
-                        this.Entity.Image = new DIKUArcade.Graphics.Image(Path.Combine("Assets", "Images", "Taxi_Thrust_Back_Right.png"));
-                        moveDir = MoveDir.Right;
-                        break;
-                    case "BOOSTER_UPWARDS":
-                        moveDir = MoveDir.Up;
-                        switch (value) {
->>>>>>> ff80e1873717e6fd7e6e651c760e77bcb241ebbc
                             case Orientation.Left:
                                 this.AddAcceleration(new Vec2F(0.0000f, 0.0001f), UpdatesSinceLastMovement);
                                 playerStrides = ImageStride.CreateStrides(2,
@@ -216,55 +186,26 @@ namespace SpaceTaxi_1.Entities {
                         }
                         break;
                     case "BOOSTER_UP_LEFT":
-                        moveDir = MoveDir.LeftUp;
-                        this.Entity.Image = new DIKUArcade.Graphics.Image(Path.Combine("Assets", "Images", "Taxi_Thrust_Bottom_Back.png"));
+                        this.AddAcceleration(new Vec2F(-0.0002828f, 0.0002828f), UpdatesSinceLastMovement);
                         break;
                     case "BOOSTER_UP_RIGHT":
-                        moveDir = MoveDir.RightUp;
-                        this.Entity.Image = new DIKUArcade.Graphics.Image(Path.Combine("Assets", "Images", "Taxi_Thrust_Bottom_Back_Right.png"));
+                        this.AddAcceleration(new Vec2F(0.0002828f, 0.0002828f), UpdatesSinceLastMovement);
                         break;
                     case "STOP_ACCELERATE_LEFT":
-                        if (moveDir == MoveDir.LeftUp) {
-                            moveDir = MoveDir.Up;
-                        } else {
-                        moveDir = MoveDir.None;
-                        }
+                        this.AddAcceleration(new Vec2F(0f, 0f), UpdatesSinceLastMovement);
                         playerIsLeftOrRight(value);
                         break;
                     case "STOP_ACCELERATE_RIGHT":
-                        if (moveDir == MoveDir.RightUp) {
-                            moveDir = MoveDir.Up;
-                        } else {
-                        moveDir = MoveDir.None;
-                        }
+                        this.AddAcceleration(new Vec2F(0f, 0f), UpdatesSinceLastMovement);
                         playerIsLeftOrRight(value);
                         break;
                     case "STOP_ACCELERATE_UP":
-                        moveDir = MoveDir.None;
+                        this.AddAcceleration(new Vec2F(0f, 0f), UpdatesSinceLastMovement);
                         playerIsLeftOrRight(value);
                         break;
                 }
             }
         }
-<<<<<<< HEAD
-=======
-        
-        //<summary> this methode helps us make sure that the player is only running inside
-        // the screen space. So the player stops moving if it reach one of the sides. <summary/>
-        //<input> none </insput>
-        //<return> void </insput> 
-        private void Move() {
-            if (Entity.Shape.Position.X > 0f && Entity.Shape.Position.X < 0.97f) {
-                Entity.Shape.Move();
-            } else if (Entity.Shape.Position.X <= 0f && Entity.Shape.AsDynamicShape().Direction.X > 0f) {
-                Entity.Shape.Move();
-            } else if (Entity.Shape.Position.X >= 0.9f && Entity.Shape.AsDynamicShape().Direction.X < 0f) {
-                Entity.Shape.Move();
-            } else{
-                this.Direction(new Vec2F(0f, 0f));
-            }
->>>>>>> ff80e1873717e6fd7e6e651c760e77bcb241ebbc
-
-
-    }     
+    }
 }
+      
