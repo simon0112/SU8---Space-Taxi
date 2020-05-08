2dfcfc2ae7f11a4f17ed4cd0d54ccef8dc5ae656
Simon Buch Kristensen <minecraftsimon99@gmail.com>
2020-05-08 15:00:19 +0200

    preliminary XML documentation

--- Space-Taxi-1/StateMachine/GameRunning.cs
+++ Space-Taxi-1/StateMachine/GameRunning.cs
@@ -35,21 +35,33 @@ namespace SpaceTaxi_1.StateMachine {
             
             eventBus = Utilities.EventBus.GetBus();
         }
+        ///<summary>Used to find out if the game has ended</summary>
+        ///<var name="GameOverActive">The variable later checked to determine if the game is over, boolean.</var>
+        ///<returns>void</returns>
         public void GameOver() {
             GameOverActive = true;
         }
-
+        ///<summary>Resets the flag for if the game is over, such that a new game can be started without closing and opening the program</summary>
+        ///<var name="GameOverActive">The variable later checked to determine if the game is over, boolean.</var>
+        ///<returns>void</returns>
         public void resetGameOver() {
             GameOverActive = false;
         }
-
+        ///<summary>Updates all logic (collision and physics) when called</summary>
+        ///<var name="GameOverActive">Checks if the game is over, if it isn't, continues into the if statement</var>
+        ///<returns>void</returns>
         public void UpdateGameLogic() {
             if (!GameOverActive) {
                 DetectCollision();
                 UpdatePhysics();
             }
         }
-
+        ///<summary>Detects collision between entities and the player</summary>
+        ///<var name="playerShape">the dynamic shape that is the player</var>
+        ///<var name="ent">the specific shape in the given list of entities</var>
+        ///<var name="platformColl">a boolean value denoting whether the the player has collided with a platform</var>
+        ///<var name="obstacleColl">a boolean value denoting whether the the player has collided with an obstacle</var>
+        ///<returns>void</returns>
         private void DetectCollision() {
             var playerShape = level.ReturnPlayer().entity.Shape.AsDynamicShape();
             foreach (Entity ent in level.portal) {
@@ -80,7 +92,8 @@ namespace SpaceTaxi_1.StateMachine {
                 }
             }
         }
-    
+        ///<summary>Used to make the game continue to the next level</summary>
+        ///<returns>void</returns>
         private void NextLevelCall() {
             if (levelCreator.reader.MetaData[0].Contains("SHORT -N- SWEET")) {
                 Utilities.EventBus.GetBus().RegisterEvent(
@@ -91,6 +104,8 @@ namespace SpaceTaxi_1.StateMachine {
                         "LEVEL_START", "the-beach.txt"));
             }
         }
+        ///<summary>Used to send a message when the player has landed on platform</summary>
+        ///<returns>void</returns>
         private void PlayerLanded() {
             Utilities.EventBus.GetBus().RegisterEvent(
                 GameEventFactory<object>.CreateGameEventForAllProcessors(
@@ -176,18 +191,22 @@ namespace SpaceTaxi_1.StateMachine {
                 level.obstacles.RenderEntities();
             }
         }
-
+        ///<summary>Used to create the level when the level is supposed to start, also subscribes the palyer to the eventbus</summary>
+        ///<var name="level">the level that is created by the levelcreator</var>
+        ///<returns>void</returns>
         public void CreateLevel(string lvlName) {
             level = levelCreator.CreateLevel(lvlName);
             eventBus.Subscribe(GameEventType.MovementEvent, level.ReturnPlayer());
             eventBus.Subscribe(GameEventType.TimedEvent, level.ReturnPlayer());
             eventBus.Subscribe(GameEventType.PlayerEvent, level.ReturnPlayer());
         }
-
+        ///<summary>Returns the active levelcreator</summary>
+        ///<returns>void</returns>
         public LevelCreator ReturnLevelCreator() {
             return this.levelCreator;
         }
-
+        ///<summary>Returns the active level</summary>
+        ///<returns>void</returns>
         public Level ReturnLevel() {
             return this.level;
         }
