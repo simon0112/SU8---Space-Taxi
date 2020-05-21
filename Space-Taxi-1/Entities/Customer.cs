4f4f5d4e790fdc4810c530d49c3e8837f0cdd180
Simon Buch Kristensen <minecraftsimon99@gmail.com>
2020-05-21 17:14:38 +0200

    Small change to the customer class such that it didn't depend on an unused dependency

--- Space-Taxi-1/Entities/Customer.cs
+++ Space-Taxi-1/Entities/Customer.cs
@@ -9,16 +9,16 @@ using SpaceTaxi_1.Utilities;
 
 
 namespace SpaceTaxi_1.Entities {
-    public class Customer : IGameEventProcessor<object> {
+    public class Customer {
         
         public Entity entity { get; private set; }
-        private string Name;
-        public int SpawnTime;
-        public string startPlatform;
-        public string goalPlatform;
-        public int TimeLimit;
-        public int pointWorth;
-        public bool visible = false;
+        public string Name { get; private set; }
+        public int SpawnTime { get; private set; }
+        public string startPlatform { get; private set; }
+        public string goalPlatform { get; private set; }
+        public int TimeLimit { get; private set; }
+        public int pointWorth { get; private set; }
+        public bool visible;
 
         public Customer(string name, int spwnTme, string start, string end, int Limit, int worth, StationaryShape shape) {
             Name = name;
@@ -32,14 +32,11 @@ namespace SpaceTaxi_1.Entities {
                 goalPlatform = end;
             }
             
-        
+            visible = false;
+
             TimeLimit = Limit;
             pointWorth = worth;
             entity = new Entity(shape, new DIKUArcade.Graphics.Image(Path.Combine("Assets", "Images", "CustomerStandRight.png")));
-            Utilities.EventBus.GetBus().Subscribe(GameEventType.TimedEvent, this);
-        }
-
-        public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
         }
     }
 }
